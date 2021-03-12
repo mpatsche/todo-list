cbed80264dac627e28f1b587645276a462536fbf
mpatsche <42234573+mpatsche@users.noreply.github.com>
2021-03-12 08:16:32 +0100

    Files added

---
+++ todo-list-script.js
@@ -0,0 +1,32 @@
+/**
+ * A TODO list
+ * 
+ * Create new TODOs and delete the ones you have done
+ * 
+ */
+
+// DOM query on input field and add eventlistener:
+
+
+// The function insertTodo()
+
+// DOM query .todo-input
+
+// Create new element: li
+
+// Add a text node to the li - the value from input field
+
+// DOM query the ul
+
+// Add the new li as the first element in the ul
+
+// Empty the input field
+
+// Add an eventlistener on the new li
+// if it's clicked - it will be removed
+
+
+// DOM query on all the TODOs - the list items:
+
+// On each of the list items add eventlistener, which
+// removes the list items if they are clicked on:
